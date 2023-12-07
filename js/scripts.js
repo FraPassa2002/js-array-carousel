98abc9e5c63e56d729633bd762ca4863139e3f21
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2023-12-07 17:03:53 +0100

    inizio esercizio

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+/*
+
+        M1:
+    1) usare HTML per creare il progetto statico ed inserire gli stili
+    2) Rimuovere il markup statico
+    3) Costruire il container
+    4) inserire un'immagine grande al centro
+        (abbiamo così struttura base e stili pronti)
+
+
+        M2:
+    5) Rimuovere il markup statico
+    6) Inseriamoci tutte le immagini con display none
+    7) Creare una classe active che faccia vedere l'immagine
+        (servendoci dell')
+    8) array
+    9) ciclo for
+        (che concatena un)
+    10) template literal
+        (ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript)
+
+
+        M3:
+    11)click frecce 
+
+*/
+
+
+const imagesNum = [
+    './img/01.webp',     // 0
+    './img/02.webp',     // 1
+    './img/03.webp',     // 2
+    './img/04.webp',     // 3
+    './img/05.webp',     // 4
+];
+
+console.log('imagesNum', imagesNum, typeof imagesNum);
