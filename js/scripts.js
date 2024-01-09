4e1da1d4462f2cca663395eaf7935f95156ace9d
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2024-01-09 18:33:27 +0100

    esercizio completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,27 @@
-/*
-    M1:
-1) usare HTML per creare il progetto statico ed inserire gli stili
-2) Rimuovere il markup statico
-3) Costruire il container
-4) inserire un'immagine grande al centro
-    (abbiamo così struttura base e stili pronti)
 
-
-    M2:
-5) Rimuovere il markup statico
-6) Inseriamoci tutte le immagini con display none
-7) Creare una classe active che faccia vedere l'immagine
-    (servendoci dell')
-8) array
-9) ciclo for
-    (che concatena un)
-10) template literal
-    (ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript)
-
-
-    M3:
-11)click frecce 
-
-*/
-
-
-const imgList = [
-'./img/01.webp',     // 0
-'./img/02.webp',     // 1
-'./img/03.webp',     // 2
-'./img/04.webp',     // 3
-'./img/05.webp',     // 4
-];
-
-const imgContainer = document.querySelector ('.img-container');
-console.log (imgContainer);
-
-let currentSlideIndex = 0;
-console.log (currentSlideIndex);
-
-for (let i = 0; i < imgList.length; i++) {
-  console.log (imgList[i]);
-
-  imgContainer.innerHTML += 
-  `
-    <div class= "item">
-      <img src=${imglist[i]}>
-    </div>
-  `;
-
-  console.log(imgContainer);
-};
-
-let displayPics = document.getElementsByClassName("item");
-displayPics[currentSlideIndex].classList.add("activate");
-console.log (displayPics);
-
-const buttonDown = document.querySelector (".button-down");
-console.log (buttonDown);
-buttonDown.addEventListener ("click", function() {
-  console.log(currentSlideIndex);
-  if (currentSlideIndex < (displayPics.length - 1)) {
-    displayPics[currentSlideIndex].classList.remove("activate");
-    currentSlideIndex++;
-    displayPics[currentSlideIndex].classList.add("activate");
-  }
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel({interval: 500});
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+    
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
 });
-
-const buttonUp = document.querySelector (".button-up");
-console.log (buttonUp);
-buttonUp.addEventListener ("click", function() {
-  if (currentSlideIndex > 0) {
-    displayPics[currentSlideIndex].classList.remove("activate");
-    currentSlideIndex--;
-    displayPics[currentSlideIndex].classList.add("activate");
-  }
-})
-
